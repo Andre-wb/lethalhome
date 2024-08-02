999d135f687c8a839cc6f040e32bee59e94e12c5
Andre-wb <andreykaravaev640@gmail.com>
2024-08-02 16:32:22 +0800

    Update lethal_script.js

--- static/js/lethal_script.js
+++ static/js/lethal_script.js
@@ -1,1 +1,29 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const openButton = document.getElementById('open_button');
+    const removeButton = document.getElementById('remove_button');
+    const body = document.getElementById('body')
+    const links = document.querySelector('.links');
+
+    if (openButton) {
+        openButton.addEventListener('click', (event) => {
+            if (links) {
+                links.classList.add('visible');
+                openButton.classList.add('invisible')
+                removeButton.classList.add('visible')
+                body.classList.add('active')
+            }
+        });
+    }
+
+    if (removeButton) {
+        removeButton.addEventListener('click', (event) => {
+            if (links) {
+                links.classList.remove('visible');
+                openButton.classList.remove('invisible')
+                removeButton.classList.remove('visible')
+                body.classList.remove('active')
+            }
+        });
+    }
+});
 
