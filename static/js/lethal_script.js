d4d7c35fa968365aec0b8d63fc7ec06050b2e7e8
Andre-wb <andreykaravaev640@gmail.com>
2024-11-07 20:34:07 +0800

    Update lethal_script.js

--- static/js/lethal_script.js
+++ static/js/lethal_script.js
@@ -1,30 +1,28 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const openButton = document.getElementById('open_button');
-    const removeButton = document.getElementById('remove_button');
     const body = document.getElementById('body');
     const links = document.querySelector('.links');
+    const button = document.getElementById('menu');
+    const footer = document.getElementById('footer');
 
-    if (openButton) {
-        openButton.addEventListener('click', (event) => {
-            if (links) {
-                links.classList.add('visible');
-                openButton.classList.add('invisible');
-                removeButton.classList.add('visible');
-                body.classList.add('active');
-            }
-        });
-    }
-
-    if (removeButton) {
-        removeButton.addEventListener('click', (event) => {
-            if (links) {
+    if (button) {
+        button.addEventListener('click', () => {
+            if (button.classList.contains('active')) {
+                button.classList.remove('active');
                 links.classList.remove('visible');
-                openButton.classList.remove('invisible');
-                removeButton.classList.remove('visible');
                 body.classList.remove('active');
+                footer.classList.remove('active');
+            } else {
+                button.classList.add('active');
+                links.classList.add('visible');
+                body.classList.add('active');
+                footer.classList.add('active');
             }
         });
     }
 });
+
+
+
+
 
 
